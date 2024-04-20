a53b0ed99fd19f09ddd4d0c77678b0c46e564b92
altese <readme42u@gmail.com>
2024-04-20 22:10:46 +0900

    💄 컬러 추가

--- lib/common/theme/app_colors.dart
+++ lib/common/theme/app_colors.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color primary = Color(0xff56ABB1);
+  static const Color primary = Color(0xffA1C398);
+  // static const Color primary = Color(0xff56ABB1);
   static const Color bgColor = Color(0xffEBECEC);
 
   static const Color textColor = Color(0xff373737);
   static const Color bgGreyColor = Color(0xffE7E7E7);
   static const Color borderColor = Color(0xffC3C3C3);
   static const Color chipColor = Color(0xff797979);
+
+  static List<Color> imageBgColors = [
+    Colors.black,
+    AppColor.borderColor,
+    AppColor.bgGreyColor,
+    Colors.white,
+    const Color(0xffB5C0D0),
+    const Color(0xffCCD3CA),
+    const Color(0xffF5E8DD),
+    const Color(0xffEED3D9),
+  ];
 }
